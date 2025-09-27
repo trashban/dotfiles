fd029cb30bc556de6a17f2e199ab119a7e0b6938
trashban <am.byshinmarh@protonmail.com>
2025-09-28 05:33:24 +1000

    Added nix-os fish aliases

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -1,3 +1,5 @@
 alias cat bat
 alias ls lsd
 alias lst "lsd --tree"
+alias en "nvim /etc/nixos/configuration.nix"
+alias nrs "sudo nixos-rebuild switch"
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # source aliases
     source ~/.config/fish/alias.fish
 
+    # vim bindings
+    set -g fish_key_bindings fish_vi_key_bindings
+
     # let yazi perserve directory
     function y
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
