9b5fcd0fdbf5c2932aa339f99ced7b34bb9e4c34
Amberveer Shinmarh <am.byshinmarh@protonmail.com>
2025-11-04 12:17:58 +1100

    fixed errors in wallpaper changing script

--- Scripts/wpchange.sh
+++ Scripts/wpchange.sh
@@ -8,17 +8,16 @@ monitor_options=$(niri msg outputs | sed -n 's/.*(\(.*-[0-9]\)).*/\1/p')
 
 # count number of monitors available
 monitor_num=$(echo "$monitor_options" | wc -l)
-monitor_choice=
 
 # if only one output, then skip prompting
-if [[ "$monitor_num" == 1 ]]; then
+if [[ "$monitor_num" -eq 1 ]]; then
     monitor_choice="$monitor_options"
 else
     monitor_choice=$(echo "$monitor_options" | rofi -dmenu -p "Select a monitor ")
 fi
 
 # if no output selected, then quit
-if [[ -z "$monitor_options" ]]; then
+if [[ -z "$monitor_choice" ]]; then
     exit 1
 fi
 
@@ -30,14 +29,14 @@ if [[ -n "$wallpaper_choice" ]]; then
     notify-send -t 3000 "Wallpaper changed to $wallpaper_choice."
 
     # set main niri wallpaper
-    swww img --transition-duration 1 --transition-fps 60 -f CatmullRom -o "$monitor_options" "$wallpaper_dir/$wallpaper_choice"
+    swww img --transition-duration 1 --transition-fps 60 -f CatmullRom -o "$monitor_choice" "$wallpaper_dir/$wallpaper_choice"
 
     # create a temp file to hold a blurred version for each monitor
-    temp_file="$HOME/Pictures/temp-${monitor_options}.jpg"
+    temp_file="$HOME/Pictures/temp-${monitor_choice}.jpg"
 
     # blur - expensive! look into not using gaussian-blur if important
     magick "$wallpaper_dir/$wallpaper_choice" -gaussian-blur 0x5 "$temp_file"
 
     # set niri overview overlay wallpaper to the same one
-    swww img -o "$monitor_options" -n overlay "$temp_file"
+    swww img -o "$monitor_choice" -n overlay "$temp_file"
 fi
