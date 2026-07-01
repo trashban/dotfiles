5cb621ff3d8ced101d050dcb080ff31c58f48b35
trashban <am.byshinmarh@protonmail.com>
2026-07-01 23:56:47 +1000

    update

--- hypr/.config/hypr/default.lua
+++ hypr/.config/hypr/default.lua
@@ -4,5 +4,8 @@ return {
     browser = "firefox-bin",
     fileManager = "yazi",
     menu = "rofi -show drun",
-    audio = "pavucontrol"
+    audio = "pavucontrol",
+    wp = "~/.scripts/wp.sh",
+    screenshot = "grim -g \"$(slurp -d)\" - | wl-copy",
+    clipboard = "cliphist list | rofi -dmenu | cliphist decode | wl-copy"
 }
